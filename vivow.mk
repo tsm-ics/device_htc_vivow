23dceba64184cc90b990d89ea1fca15ac18ec856
OMJ <mijames1@gmail.com>
2011-08-31 08:16:59 -0300

    other prop overrides from cm vendor

--- vivow.mk
+++ vivow.mk
@@ -58,6 +58,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.telephony.support_ipv6=true \
     persist.telephony.support_ipv4=true
 
+#Additional Overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false
+
 DEVICE_PACKAGE_OVERLAYS += device/htc/vivow/overlay
 
 # gsm config xml file
