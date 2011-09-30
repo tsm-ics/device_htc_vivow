b635fdf95a7a8d1bac3adc45b224792371d2d880
attn1 <attn1.repo@gmail.com>
2011-09-30 10:15:10 -0400

    fix
    Change-Id: Iaf603cd85e1fb1dfa59498b43eec1a4fdcfa1578

--- extract-files.sh
+++ extract-files.sh
@@ -78,14 +78,9 @@ adb pull /system/lib/libqdp.so ../../../vendor/$MANUFACTURER/$DEVICE/proprietary
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \\
     vendor/__MANUFACTURER__/__DEVICE__/proprietary/libcamera.so:obj/lib/libcamera.so \\
-<<<<<<< HEAD
     vendor/__MANUFACTURER__/__DEVICE__/proprietary/libaudio.so:obj/lib/libaudio.so \\
     vendor/__MANUFACTURER__/__DEVICE__/proprietary/libaudioalsa.so:obj/lib/libaudioalsa.so \\
     vendor/__MANUFACTURER__/__DEVICE__/proprietary/libril.so:obj/lib/libril.so
-=======
-    vendor/__MANUFACTURER__/__DEVICE__/proprietary/libril.so:obj/lib/libril.so \\
-    vendor/__MANUFACTURER__/__DEVICE__/proprietary/libaudioalsa.so:obj/lib/libaudioalsa.so
->>>>>>> 17b8dda... vivow: switch to htc ril
 
 # All the blobs necessary for vivow
 PRODUCT_COPY_FILES += \\
